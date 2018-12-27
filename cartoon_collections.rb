e7fe84e7bb9b88a04ff430b103c7fe3f2bf45194
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 09:34:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr) # code an argument here
   # Your code here
+  arr.each_with_index do |ele, i|
+    position = i + 1 
+    puts "#{position}. #{ele}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr) # code an argument here
   # Your code here
+  arr.map do |ele|
+    "#{ele[0].upcase}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr) # code an argument here
+  # Yaur code here
+  arr.any? do |ele| ele.length > 4 
+    end
 end
 
 def find_the_cheese# code an argument here
